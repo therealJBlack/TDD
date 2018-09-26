0c3f20354deb1ea390bf495029c6791b2409d186
Josh Black <josh.black1500@gmail.com>
2018-09-25 21:29:30 -0700

    updated testing for chapter4

--- lists/tests.py
+++ lists/tests.py
@@ -1,24 +1,18 @@
 from django.urls import resolve
+from django.template.loader import render_to_string
 from django.test import TestCase
 from django.http import HttpRequest
-
 from lists.views import home_page
 
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-
     def test_home_page_returns_correct_html(self):
-        #Arrange
-        request = HttpRequest()  
-        response = home_page(request)
-        #Act
-        html = response.content.decode('utf8')
-        #Assert
-        self.assertTrue(html.startswith('<html>'))  
-        self.assertIn('<title>To-Do lists</title>', html)  
-        self.assertTrue(html.endswith('</html>'))  
+        response = self.client.get('/')  
+
+        html = response.content.decode('utf8')  
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')  
