52f9aef97d8414d7110b75262421f9ad99d29db8
Josh Black <josh.black1500@gmail.com>
2018-09-25 21:45:57 -0700

    Front page HTML now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -7,6 +7,10 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')  
 
